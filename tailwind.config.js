de15e9358d84fe7b1001377028a739fe303f723a
Taahirah Denmark <126827805+Engineernoob@users.noreply.github.com>
2025-07-06 13:52:36 -0500

    css config files

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      // adjust these globs to match your source files
+      "./index.html",
+      "./src/**/*.{js,jsx,ts,tsx}"
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
